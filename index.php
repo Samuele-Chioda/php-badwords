84ed5db48a64622a418b099168adc2782bb42664
Samuele-Chioda <samuelechioda@gmail.com>
2024-06-25 16:52:37 +0200

    sistemazione struttura e sostituzione paragrafo

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+        $paragrafo = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,";
+
+            $paroleSost = str_replace('lorem', '***', $paragrafo);
+            $numeroLettere = strlen($paragrafo);
+        ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,44 +17,10 @@
 
 <body>
     <main>
-        <?php
-        $paragrafo = "Nel mezzo del cammin di nostra vita
-                mi ritrovai per una selva oscura,
-                ché la diritta via era smarrita.
-                Ahi quanto a dir qual era è cosa dura
-                esta selva selvaggia e aspra e forte
-                che nel pensier rinova la paura!
-                Tant’è amara che poco è più morte;
-                ma per trattar del ben ch’i’ vi trovai,
-                dirò de l’altre cose ch’i’ v’ ho scorte.
-                Io non so ben ridir com’i’ v’intrai,
-                tant’era pien di sonno a quel punto
-                che la verace via abbandonai. Nel mezzo del cammin di nostra vita
-                mi ritrovai per una selva oscura,
-                ché la diritta via era smarrita.
-                Ahi quanto a dir qual era è cosa dura
-                esta selva selvaggia e aspra e forte
-                che nel pensier rinova la paura!
-                Tant’è amara che poco è più morte;
-                ma per trattar del ben ch’i’ vi trovai,
-                dirò de l’altre cose ch’i’ v’ ho scorte.
-                Io non so ben ridir com’i’ v’intrai,
-                tant’era pien di sonno a quel punto
-                che la verace via abbandonai. Nel mezzo del cammin di nostra vita
-                mi ritrovai per una selva oscura,
-                ché la diritta via era smarrita.
-                Ahi quanto a dir qual era è cosa dura
-                esta selva selvaggia e aspra e forte
-                che nel pensier rinova la paura!
-                Tant’è amara che poco è più morte;
-                ma per trattar del ben ch’i’ vi trovai,
-                dirò de l’altre cose ch’i’ v’ ho scorte.
-                Io non so ben ridir com’i’ v’intrai,
-                tant’era pien di sonno a quel punto
-                che la verace via abbandonai.";
-                $paragrafo = str_replace('selva', '***', $paragrafo);
-        echo $paragrafo;
-        ?>
+        <?php echo $paroleSost ?>
+        <div class="Nlettere">
+            <?php echo 'la lunghezza del paragrafo è di' . $numeroLettere .'lettere'; ?>
+        </div>
     </main>
 </body>
 
