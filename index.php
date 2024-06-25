50c1282017575cba9a68835cf1aec05f89ede633
Samuele-Chioda <samuelechioda@gmail.com>
2024-06-25 13:36:24 +0200

    aggiunta variabile paragrafo

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Badwords</title>
 </head>
+
 <body>
     <main>
-        
+        <?php
+        $paragrafo = "ciao ciao ciao ciao";
+        echo $paragrafo;
+        ?>
     </main>
 </body>
+
 </html>
