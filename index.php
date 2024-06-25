c6103358630b7647b0288a8afb74369100576ac6
Samuele-Chioda <samuelechioda@gmail.com>
2024-06-25 16:04:19 +0200

    aggiunto paragrafo

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-
 <html lang="en">
 
 <head>
@@ -11,7 +10,18 @@
 <body>
     <main>
         <?php
-        $paragrafo = "ciao ciao ciao ciao";
+        $paragrafo = "Nel mezzo del cammin di nostra vita
+                mi ritrovai per una selva oscura,
+                ché la diritta via era smarrita.
+                Ahi quanto a dir qual era è cosa dura
+                esta selva selvaggia e aspra e forte
+                che nel pensier rinova la paura!
+                Tant’è amara che poco è più morte;
+                ma per trattar del ben ch’i’ vi trovai,
+                dirò de l’altre cose ch’i’ v’ ho scorte.
+                Io non so ben ridir com’i’ v’intrai,
+                tant’era pien di sonno a quel punto
+                che la verace via abbandonai.";
         echo $paragrafo;
         ?>
     </main>
